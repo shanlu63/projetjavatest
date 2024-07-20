36b3209fa03d941759b1d5533dfbbbdcfcfe7747
shanlu <40007160@parisnanterre.fr>
2024-07-20 20:01:35 +0200

    créer un projet java pour tester

---
+++ projetjavatest/src/main/java/com/mycompany/projetjavatest/Projetjavatest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetjavatest;
+
+/**
+ *
+ * @author shan
+ */
+public class Projetjavatest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("c'est le projet java test par Shan LU et kayiba");
+    }
+}
