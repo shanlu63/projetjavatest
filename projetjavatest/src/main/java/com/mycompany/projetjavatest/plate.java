69533048b4c92dedcbc895b6b9003ae9e60d1484
shanlu <40007160@parisnanterre.fr>
2024-07-25 15:06:06 +0200

    créer un projet java pour tester

---
+++ projetjavatest/src/main/java/com/mycompany/projetjavatest/plate.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetjavatest;
+
+/**
+ *
+ * @author shan
+ */
+public class plate {
+        int index;//index de plate
+        String NomPlate;//nom de plate
+        String type;
+        String prix;
+        
+        //construction
+        
+        
+}
